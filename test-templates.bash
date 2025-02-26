da4281f840435d7279737361aadb78dd9b1d3645
Victor Borja <vborja@apache.org>
2025-02-25 21:05:31 -0600

    Show generated bin directory

--- test-templates.bash
+++ test-templates.bash
@@ -4,25 +4,23 @@
 
 set -euo pipefail
 
-out="$(mktemp -d)"
+for template in templates/*; do
 (
+    echo "=========== $template ==========="
+    out="$(mktemp -d)"
     export HOME="$out"
     mkdir -p "$HOME/.config/nix"
     echo "extra-experimental-features = nix-command flakes" > "$HOME/.config/nix/nix.conf"
 
     nix run .
     test -e "$HOME/.config/direnv/lib/use_devshell_toml.sh"
-    eval "$(direnv hook bash)"
 
-    for template in templates/*; do
-    (
-        echo "=========== $template ==========="
-        cd "$template"
-        direnv allow 
-        direnv exec "$PWD" direnv dump json | jq -r .DEVSHELL_DIR | tee "$out/$(basename "$template")"
-        devshell_bin="$(< "$out/$(basename "$template")")/bin"
-        ls -l "$devshell_bin"
-        "$devshell_bin/menu"
-    )
-    done
+    eval "$(direnv hook bash)"
+    cd "$template"
+    direnv allow 
+    direnv exec "$PWD" direnv dump json | jq -r .DEVSHELL_DIR | tee "$out/$(basename "$template")"
+    devshell_bin="$(< "$out/$(basename "$template")")/bin"
+    ls -l "$devshell_bin"
+    "$devshell_bin/menu"
 )
+done
