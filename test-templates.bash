fb4be1b6fb4a6972abad561016b02b0db0a652a1
Victor Borja <vborja@apache.org>
2025-02-28 16:51:29 -0600

    Dont use tmp on CI

--- test-templates.bash
+++ test-templates.bash
@@ -4,33 +4,38 @@
 
 base="$PWD"
 function test_template() {
-    set -vaeuo pipefail
-    env
-    template="$1"
-    echo "===== $template =====" | tr '[:print:]' '='
-    echo "||    $template    ||"
-    echo "===== $template =====" | tr '[:print:]' '='
+  set -vaeuo pipefail
+  env
+  template="$1"
+  echo "===== $template =====" | tr '[:print:]' '='
+  echo "||    $template    ||"
+  echo "===== $template =====" | tr '[:print:]' '='
+  if test -z "${CI:-}"; then
     out="$(mktemp -d)"
     export HOME="$out"
-    mkdir -p "$HOME/.config/nix"
-    echo "extra-experimental-features = nix-command flakes" > "$HOME/.config/nix/nix.conf"
+  else
+    out="$HOME"
+  fi
+  mkdir -p "$HOME/.config/nix"
+  echo "extra-experimental-features = nix-command flakes" >"$HOME/.config/nix/nix.conf"
 
-    cd "$out"
+  cd "$out"
 
-    cp -rf "$base/$template"/* "$out/"
+  cp -rf "$base/$template"/* "$out/"
 
-    nix run "path:$base" --show-trace
-    test -e "$HOME/.config/direnv/lib/use_devshell_toml.sh"
+  nix run "path:$base" --show-trace
+  test -e "$HOME/.config/direnv/lib/use_devshell_toml.sh"
 
-    # use bash strict inside .envrc
-    echo "set -euo pipefail; source $HOME/.config/direnv/lib/use_devshell_toml.sh; use devshell_toml --show-trace" > "$out/.envrc" && direnv allow "$out"
-    direnv exec "$out" check
+  # use bash strict inside .envrc
+  echo "set -euo pipefail; source $HOME/.config/direnv/lib/use_devshell_toml.sh; use devshell_toml --show-trace" >"$out/.envrc" && direnv allow "$out"
+  direnv exec "$out" check
 }
 
 if test -z "${1:-}"; then
-    for template in templates/*; do
-      (test_template "$template")
-    done
+  for template in templates/*; do
+    test_template "$template"
+  done
 else
-  (test_template "$1")
+  test_template "$1"
 fi
+
